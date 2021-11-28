5ab9b5bcf89780693c5c988e8f5e61108de4cd75
gustibilli <85354854+SentientCatWynde@users.noreply.github.com>
2021-11-28 21:44:21 +0700

    mockup_pertanyaan

---
+++ diagnosisGigi/src/components/localVars.tsx
@@ -0,0 +1,39 @@
+export const QuestionList = [
+    {id: 'p1',   data: 'Apa mulut memiliki bau tidak sedap?'},
+    {id: 'p2',   data: 'Apa Anda mengalami demam?'},
+    {id: 'p3',   data: 'Apa gusi Anda bengkak?'},
+    {id: 'p4',   data: 'Apa gusi Anda bengkak dan berdarah?'},
+    {id: 'p5',   data: 'Apa anda memiliki luka antara gusi dan gigi?'},
+    {id: 'p6',   data: 'Apa anda memiliki benjolan di bagian rahang?'},
+    {id: 'p7',   data: 'Apa gusi Anda mudah berdarah?'},
+    {id: 'p8',   data: 'Apa mulut Anda nyeri saat makan atau minum?'},
+    {id: 'p9',   data: 'Apa mulut anda sakit saat mengunyah makanan?'},
+    {id: 'p10',  data: 'Apa terdapat cekungan dalam gigi Anda?'},
+    {id: 'p11',  data: 'Apa terdapat noda aneh pada gigi Anda?'},
+    {id: 'p12',  data: 'Apa Anda memiliki bercak/bintik merah atau putih dalam mulut?'},
+    {id: 'p13',  data: 'Apa gigi Anda memiliki kerak yang berlebihan?'},
+    {id: 'p14',  data: 'Apa gusi Anda berwarna kemerahan atau keunguan?'},
+    {id: 'p15',  data: 'Apa nanah muncul dalam mulut Anda?'},
+    {id: 'p16',  data: 'Apa gusi Anda nyeri pada saat ini?'}
+];
+
+
+export const Answerlist = [
+    {id: 'p1',  value: 'no-answer'},
+	{id: 'p2',  value: 'no-answer'},
+	{id: 'p3',  value: 'no-answer'},
+	{id: 'p4',  value: 'no-answer'},
+	{id: 'p5',  value: 'no-answer'},
+	{id: 'p6',  value: 'no-answer'},
+	{id: 'p7',  value: 'no-answer'},
+	{id: 'p8',  value: 'no-answer'},
+	{id: 'p9',  value: 'no-answer'},
+	{id: 'p10',  value: 'no-answer'},
+	{id: 'p11',  value: 'no-answer'},
+	{id: 'p12',  value: 'no-answer'},
+	{id: 'p13',  value: 'no-answer'},
+	{id: 'p14',  value: 'no-answer'},
+	{id: 'p15',  value: 'no-answer'},
+	{id: 'p16',  value: 'no-answer'}
+];
+
---
+++ diagnosisGigi/src/pages/Home.tsx
@@ -0,0 +1,90 @@
+import { IonCol, IonLabel, IonRadio, IonContent, IonFooter, IonGrid, IonHeader, IonItem, IonPage, IonRadioGroup, IonRow, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import { useState } from 'react';
+import { QuestionList } from '../components/localVars';
+
+import './Home.css';
+
+const Home: React.FC = () => {
+  const [ selected, setSelected ] = useState<string>('no-answer');
+  const [ question, setQuestion ] = useState<string>(QuestionList[0].data);
+  const [ i, seti ] = useState<number>(1);
+  /* var j:number = 0; */
+
+  const nextQBtnHandler = () => {
+    console.log(i);
+    if (i >= 10){
+      console.log('maximum question');
+    } else {
+      seti(i+1);
+      setQuestion(QuestionList[i].data);
+    }
+    console.log(i);
+    
+  }
+  /* const prevQBtnHandler = () => {
+    console.log(i);
+    if (i <= 0){
+      console.log('min question');
+    } else {
+      seti(i-1);
+      setQuestion(QuestionList[i].data);
+    }
+    console.log(i);
+  } */
+
+  return (
+    <IonPage>
+      <IonHeader >
+        <IonToolbar color = 'primary'>
+          <IonTitle> SISTEM PAKAR TES KESEHATAN GIGI BERBASIS WEB </IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen >
+      
+
+        <IonGrid class = 'ion-padding'>
+          <IonRow>
+            <IonCol> 
+              <IonTitle> Pertanyaan </IonTitle>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol> 
+              <IonTitle> { question } </IonTitle>
+            </IonCol>
+          </IonRow>
+          <br/>
+          <IonRow>
+            <IonCol> 
+              <IonRadioGroup onIonChange={e => setSelected(e.detail.value)}>
+                <IonItem>
+                  <IonLabel>True</IonLabel>
+                  <IonRadio value="true" />
+                </IonItem>
+                <IonItem>
+                  <IonLabel>Fale</IonLabel>
+                  <IonRadio value="false" />
+                </IonItem>
+              </IonRadioGroup>
+            </IonCol>
+          </IonRow>
+          <IonRow class = 'ion-padding'>
+          
+            <IonCol>
+              <IonButton onClick = { nextQBtnHandler }>
+                Next Question
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+      <IonFooter  color = 'primary' >
+        <IonToolbar color = 'primary'>
+        <IonTitle> By : G9-SiskarTG_IF541_C</IonTitle>
+        </IonToolbar>
+      </IonFooter>
+    </IonPage>
+  );
+};
+
+export default Home;
